fb4b97cadc15a65c5ad52da44ffa48011852fbb9
Nelson Chen <nelsonchen5@gmail.com>
2018-03-15 11:05:16 -0700

    changes

--- server/rest-server/index.js
+++ server/rest-server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(express.static(path.join(__dirname, "../client/public")))
+app.use(express.static(path.join(__dirname, "../../spotifight-ui/public")))
 
 app.use(router)
 
