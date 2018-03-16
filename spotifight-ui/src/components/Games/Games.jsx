1d8eeb397625ed0d36aa3678c2f9909aa7f13a2d
Johnny Siu <siujohnny@gmail.com>
2018-03-16 10:55:17 -0700

    rebasing

--- spotifight-ui/src/components/Games/Games.jsx
+++ spotifight-ui/src/components/Games/Games.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Hello from './Hello.jsx';
 
 export default class Games extends Component {
   render() {
-    return <div> <Hello /></div>;
+    return <div> </div>;
   }
 }
