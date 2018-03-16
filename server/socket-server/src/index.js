61f6ba285c914b76d73f3c85b27cb6aff547b079
Ryan Santos <ryansantos86@gmail.com>
2018-03-16 14:43:57 -0700

    (feat) basic chat working using sockets

--- server/socket-server/src/index.js
+++ server/socket-server/src/index.js
@@ -1,27 +1,46 @@
-const express = require('express')
-const app = express()
-const http = require('http').Server(app)
-const io= require('socket.io')(http)
-
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // const rooms = new Rooms(io);
 
-io.on('connection', (client) => {
-  // success('client connected');
-  // const { roomId } = client.handshake.query;
-  // const room = rooms.findOrCreate(roomId || 'default');
-  // client.join(room.get('id'));
+let users = [];
+let connections = [];
 
-  // each(clientEvents, (handler, event) => {
-  //   client.on(event, handler.bind(null, { io, client, room }));
-  // });
-  client.join('lobby', )
-  client.on('message', (message) => {
-    io.emit('serverMessage', message)
-    console.log('message received!!')
-  })
-  console.log('a user has connected to socket server')
+io.on('connection', socket => {
+  connections.push(socket);
+  console.log(`Connected %s sockets connected ${connections.length}`);
+
+  // disconnect
+  socket.on('disconnect', data => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnected: %s sockets connected ${connections.length}`);
+  });
+
+  // send message
+  socket.on('send message', data => {
+    console.log(data);
+    io.sockets.emit('new message', { msg: data });
+  });
 });
 
-const PORT = 8000
+// io.on('connection', client => {
+//   // success('client connected');
+//   // const { roomId } = client.handshake.query;
+//   // const room = rooms.findOrCreate(roomId || 'default');
+//   // client.join(room.get('id'));
+
+//   // each(clientEvents, (handler, event) => {
+//   //   client.on(event, handler.bind(null, { io, client, room }));
+//   // });
+//   client.join('lobby');
+//   client.on('message', message => {
+//     io.emit('serverMessage', message);
+//     console.log('message received!!');
+//   });
+//   console.log('a user has connected to socket server');
+// });
+
+const PORT = 8000;
 http.listen(PORT, () => console.log(`socket server listening on port ${PORT}`));
--- spotifight-ui/src/components/Chat/Chat.jsx
+++ spotifight-ui/src/components/Chat/Chat.jsx
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
-import { Button, TextField } from './../Global/Material-Globals';
-import Grid from 'material-ui/Grid';
+import io from 'socket.io-client';
+import {
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  Icon
+} from './../Global/Material-Globals';
+import './Chat.scss';
+
+let socket = io.connect('http://localhost:8000');
 
 export default class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      textField: '',
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    socket.on('new message', data => {
+      this.setState({
+        messages: this.state.messages.concat([data.msg])
+      });
+    });
+  }
+
+  handleSend() {
+    this.state.textField
+      ? socket.emit('send message', this.state.textField)
+      : console.log('text field empty');
+    this.setState({ textField: '' });
+  }
+
+  setTextField(e) {
+    const name = e.target.name;
+    const val = e.target.value;
+    this.setState({
+      [name]: val
+    });
+  }
+
   render() {
     return (
-      <div>
-       
+      <div className="chat">
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={4}>
+            <Paper>users go here</Paper>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <div>
+              {this.state.messages.map((message, index) => {
+                return <Paper key={index}>{message}</Paper>;
+              })}
+            </div>
+            <TextField
+              className="text-field"
+              label="message"
+              placeholder="enter message"
+              multiline
+              margin="normal"
+              name="textField"
+              value={this.state.textField}
+              onChange={e => this.setTextField(e)}
+            />
+            <Button onClick={() => this.handleSend()}>send</Button>
+          </Grid>
+        </Grid>
       </div>
     );
   }
