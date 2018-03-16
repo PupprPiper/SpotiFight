956373299a307360bf003b93a0120bda434f8475
Nelson Chen <nelsonchen5@gmail.com>
2018-03-16 10:49:56 -0700

    more home changes

--- spotifight-ui/src/components/Home/Home.jsx
+++ spotifight-ui/src/components/Home/Home.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 export default class Home extends Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  handleGameRoomRedirect(){
+
+    this.props.history.push({pathname:'/game-room'})
+  }
   render() {
     return <div>
       
@@ -9,7 +18,7 @@ export default class Home extends Component {
       Hello from Home
       
       
-      <input type ='submit' value = 'CREATE'/> 
+      <input type ='submit' value = 'CREATE' onClick = {() => this.handleGameRoomRedirect()}/> 
       
       </div>;
   }
