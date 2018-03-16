8962d2bb3fe1f64701b770b85bf08ff2dc1eca7f
Michael Wachell <wachell@gmail.com>
2018-03-16 10:53:18 -0700

    fixed games

--- spotifight-ui/src/components/Games/Games.jsx
+++ spotifight-ui/src/components/Games/Games.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Hello from './Hello.jsx';
+
 
 export default class Games extends Component {
   render() {
-    return <div> <Hello /></div>;
+    return <div> </div>;
   }
 }
