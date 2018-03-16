c6266736567a9fada02dd61cf07ce912c91ca017
Ryan Santos <ryansantos86@gmail.com>
2018-03-16 15:58:15 -0700

    commit

--- server/socket-server/src/index.js
+++ server/socket-server/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require('babel-register')
 require('babel-polyfill')
 const express = require('express')
@@ -5,6 +6,12 @@ const app = express()
 const http = require('http').Server(app)
 const io= require('socket.io')(http)
 
+=======
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+>>>>>>> (feat) basic chat working using sockets
 
 class Rooms {
   constructor(io) {
@@ -28,6 +35,7 @@ class Rooms {
 const rooms = new Rooms(io)
 
 
+<<<<<<< HEAD
 io.on('connection', (client) => {
   // success('client connected');
   // const { roomId } = client.handshake.query;
@@ -50,7 +58,44 @@ io.on('connection', (client) => {
     console.log('this is our room', room)
   })
   console.log('a user has connected to socket server')
+=======
+let users = [];
+let connections = [];
+
+io.on('connection', socket => {
+  connections.push(socket);
+  console.log(`Connected %s sockets connected ${connections.length}`);
+
+  // disconnect
+  socket.on('disconnect', data => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnected: %s sockets connected ${connections.length}`);
+  });
+
+  // send message
+  socket.on('send message', data => {
+    console.log(data);
+    io.sockets.emit('new message', { msg: data });
+  });
+>>>>>>> (feat) basic chat working using sockets
 });
 
-const PORT = 8000
+// io.on('connection', client => {
+//   // success('client connected');
+//   // const { roomId } = client.handshake.query;
+//   // const room = rooms.findOrCreate(roomId || 'default');
+//   // client.join(room.get('id'));
+
+//   // each(clientEvents, (handler, event) => {
+//   //   client.on(event, handler.bind(null, { io, client, room }));
+//   // });
+//   client.join('lobby');
+//   client.on('message', message => {
+//     io.emit('serverMessage', message);
+//     console.log('message received!!');
+//   });
+//   console.log('a user has connected to socket server');
+// });
+
+const PORT = 8000;
 http.listen(PORT, () => console.log(`socket server listening on port ${PORT}`));
