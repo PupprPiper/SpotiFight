42590bd5c36647827313ce90f99c52a7a8b032ed
Johnny Siu <siujohnny@gmail.com>
2018-03-17 12:09:37 -0700

    woring on friends

--- spotifight-ui/src/components/Friends/Friends.jsx
+++ spotifight-ui/src/components/Friends/Friends.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 export default class UserProfile extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
+      friends: [],
+      input: ''
+    };
+  }
 
-    }
+  addFriend() {
+    
   }
 
   render() {
-    return <div>Hello from Friends</div>;
+    return <div>
+      <input type='text' onChange={(e) => this.setState({input: e.target.value})}/>
+      <input type="submit" value="Add Friend" onClick={() => this.addFriend()}/>
+      </div>;
   }
 }
