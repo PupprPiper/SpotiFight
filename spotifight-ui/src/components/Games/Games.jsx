619630d65bb366de91b54e4575590ae0be475ee4
Michael Wachell <wachell@gmail.com>
2018-03-17 10:29:02 -0700

    game runs

--- spotifight-ui/src/components/Games/Games.jsx
+++ spotifight-ui/src/components/Games/Games.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import Masher from './Masher/Masher.jsx';
 >>>>>>> plays basic front end functionality for Masher.game
@@ -16,6 +17,9 @@ import Masher from './Masher/Masher.jsx';
 =======
 import Masher from './Masher/Masher.jsx';
 >>>>>>> plays basic front end functionality for Masher.game
+=======
+import Masher from './Masher/Masher.jsx';
+>>>>>>> game runs
 
 export default class Games extends Component {
   render() {
