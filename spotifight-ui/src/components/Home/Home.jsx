b81a56ae4eafece71ac0931bddad099bb4226c2c
Nelson Chen <nelsonchen5@gmail.com>
2018-03-17 11:07:35 -0700

    game rooms and switcher

--- spotifight-ui/src/components/Home/Home.jsx
+++ spotifight-ui/src/components/Home/Home.jsx
@@ -3,15 +3,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import randomstring from "randomstring";
 import io from "socket.io-client";
 
-
+let randomRoom;
 export default class Home extends Component {
   constructor() {
     super();
     this.state = {
       socket: null,
-      // game: "testgame",
-      // roomsOpen: {},
-      roomName: null
+      // roomName: null
     }
   }
   componentDidMount() {
@@ -19,43 +17,54 @@ export default class Home extends Component {
     // this.setState({ socket: this.socket });
   }
 
-  // randomRoom() {
-  //   randomRoom = `${randomstring.generate()}`;
-  // }
+  randomRoom() {
+    randomRoom = `${randomstring.generate()}`;
+  }
   handleGameRoomRedirect() {
-    // this.randomRoom();
-    // this.props.history.push({
-    //   pathname: `/game-room/${randomRoom}`,
-    //   state: { game: this.state.game }
-    // });
+    this.randomRoom();
+    // this.socket = io.connect("http://localhost:8000", {
+    //     query: {
+    //       roomId: randomRoom
+    //     }
+    //   })
+    //   this.setState({ socket: this.socket });
+    
+
+    
+    this.props.history.push({
+      pathname: `/game-room/${randomRoom}`,
+      state: { game: this.state.game }
+    });
     
   }
 
-  joinRoom(){
-      // this.socket.emit('joinRoom', this.state.roomName)
-      this.socket = io.connect("http://localhost:8000", {
-        query: {
-          roomName: this.state.roomName
-        }
-      })
-      this.setState({ socket: this.socket });
-      this.props.history.push({pathname: '/chat'})
+  // joinRoom(){
+  //     // this.socket.emit('joinRoom', this.state.roomName)
+  //     this.socket = io.connect("http://localhost:8000", {
+  //       query: {
+  //         roomName: this.state.roomName
+  //       }
+  //     })
+  //     this.setState({ socket: this.socket });
+  //     this.props.history.push({pathname: '/'})
     
-    }
+  //   }
   render() {
     return (
       <div>
+
+        {console.log(this.props.location)}
         {/* {console.log(this.props)} */}
         Hello from Home
-        <input
+        {/* <input
           id='join'
           type="text"
           onChange={(e) => this.setState({roomName: e.target.value})}
-        />
+        /> */}
         <input
           type="submit"
-          value="JOIN"
-          onClick={() => this.joinRoom()}
+          value="CREATE A NEW ROOM"
+          onClick={() => this.handleGameRoomRedirect()}
         />
       </div>
     );
