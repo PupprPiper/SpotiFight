28894d9ab8cb427c3ffd2fa162d8d6101cc5a487
Nelson Chen <nelsonchen5@gmail.com>
2018-03-16 19:45:27 -0700

    rebase changes

--- server/socket-server/src/index.js
+++ server/socket-server/src/index.js
@@ -13,44 +13,23 @@ const io= require('socket.io')(http)
 
 
 
-class Rooms {
-  constructor(io) {
-    this.io = io;
-    this.store = new Map();
-    this.findOrCreate = this.findOrCreate.bind(this)
-  }
-
-  findOrCreate(roomId) {
-    let room = this.store.get(roomId);
-    if (room) {
-      room = new Map();
-      room.set('id', roomId);
-      room.set('text', startingText);
-      this.store.set(roomId, room);
-    }
-    return room;
-  }
-}
-
-const rooms = new Rooms(io)
-
 
 
 let users = [];
 let connections = [];
 
-io.on('connection', socket => {
-  connections.push(socket);
-  console.log(`Connected %s sockets connected ${connections.length}`);
+io.on('connection', client => {
+  connections.push(client);
+  console.log(`Connected %s clients connected ${connections.length}`);
 
   // disconnect
-  socket.on('disconnect', data => {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log(`Disconnected: %s sockets connected ${connections.length}`);
+  client.on('disconnect', data => {
+    connections.splice(connections.indexOf(client), 1);
+    console.log(`Disconnected: %s clients connected ${connections.length}`);
   });
 
   // send message
-  socket.on('send message', data => {
+  client.on('send message', data => {
     console.log(data);
     io.sockets.emit('new message', { msg: data });
   });
@@ -70,7 +49,7 @@ io.on('connection', (client) => {
   client.join(client.handshake.query.roomId)
 
 
-  client.on('message', (message) => {
+  client.on('game', (message) => {
     io.emit('serverMessage', message)
     console.log('message received!!')
     console.log(client.handshake.query)
@@ -79,23 +58,6 @@ io.on('connection', (client) => {
   console.log('a user has connected to socket server')
 });
 })
-
-// io.on('connection', client => {
-//   // success('client connected');
-//   // const { roomId } = client.handshake.query;
-//   // const room = rooms.findOrCreate(roomId || 'default');
-//   // client.join(room.get('id'));
-
-//   // each(clientEvents, (handler, event) => {
-//   //   client.on(event, handler.bind(null, { io, client, room }));
-//   // });
-//   client.join('lobby');
-//   client.on('message', message => {
-//     io.emit('serverMessage', message);
-//     console.log('message received!!');
-//   });
-//   console.log('a user has connected to socket server');
-// });
 
 const PORT = 8000;
 http.listen(PORT, () => console.log(`socket server listening on port ${PORT}`));
