8af979d81ed3eae96dfd11df96823f3cc0c6fc38
Johnny Siu <siujohnny@gmail.com>
2018-03-17 15:18:02 -0700

    add deleteFriendHelper

--- server/rest-server/components/Friends/friendsSQLHelpers.js
+++ server/rest-server/components/Friends/friendsSQLHelpers.js
@@ -2,9 +2,19 @@ module.exports  = {
   addFriendHelper: (req) => {
     return `
       INSERT INTO friends (user_id, friend_id)
-      VALUES (${req.body.user_id}, ${req.body.friend_id}), (${req.body.friend_id}, ${req.body.user_id})
+      VALUES (${req.body.user_id}, ${req.body.friend_id}), 
+      (${req.body.friend_id}, ${req.body.user_id})
       RETURNING id, user_id, friend_id
     `
+  },
+
+  deleteFriendHelper: (req) => {
+    return `
+    DELETE FROM friends
+    WHERE (user_id=${req.body.user_id} AND friend_id=${req.body.friend_id}) OR 
+    (user_id=${req.body.friend_id} AND friend_id=${req.body.user_id})
+    RETURNING id, user_id, friend_id
+  `
   }
 
 }
