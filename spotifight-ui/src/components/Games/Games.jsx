c7d741e0d4ee5ae9250788c866e89037be05f290
Nelson Chen <nelsonchen5@gmail.com>
2018-03-17 11:14:20 -0700

    nelson changes

--- spotifight-ui/src/components/Games/Games.jsx
+++ spotifight-ui/src/components/Games/Games.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import Masher from './Masher/Masher.jsx';
 >>>>>>> plays basic front end functionality for Masher.game
 =======
 >>>>>>> plays basic front end functionality for Masher.game
+=======
+=======
+import Masher from './Masher/Masher.jsx';
+>>>>>>> plays basic front end functionality for Masher.game
+>>>>>>> hehe
 =======
 import Masher from './Masher/Masher.jsx';
 >>>>>>> plays basic front end functionality for Masher.game
