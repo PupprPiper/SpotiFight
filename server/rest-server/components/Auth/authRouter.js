356731f5239cd9c65b25ecc759b723305fc8e451
Ryan Santos <ryansantos86@gmail.com>
2018-03-18 20:24:23 -0700

    (feat) retrieving google profile data

---
+++ server/rest-server/components/Auth/authRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const passport = require('passport');
+const {
+  googleLoginCtrl,
+  googleRedirectCtrl,
+  logoutCtrl
+} = require('./authController');
+
+// google auth
+router.route('/google').get(googleLoginCtrl);
+
+// callback route for google to redirect to
+router
+  .route('/google/redirect')
+  .get(passport.authenticate('google'), googleRedirectCtrl);
+
+// logout
+router.route('/logout').get(logoutCtrl);
+
+module.exports = router;
--- server/rest-server/routes/index.js
+++ server/rest-server/routes/index.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 const userRouter = require('../components/User/userRouter');
 const friendsRouter = require('../components/Friends/friendsRouter')
 const spotifyRouter = require('../components/Spotify/spotifyRouter')
+const authRouter = require('./../components/Auth/authRouter')
 //to add the rest later
 
+// user route
 router.use('/users', userRouter);
 router.use('/friends', friendsRouter);
 // router.use('/songs', songRouter);
@@ -13,6 +15,8 @@ router.use('/friends', friendsRouter);
 
 router.use('/spotify', spotifyRouter);
 
+// auth route
+router.use('/auth', authRouter);
 
 
 
