c9ed4ebfb5c9adb408fa0d4c1d45157a2cb46bd4
Nelson Chen <nelsonchen5@gmail.com>
2018-03-21 09:42:03 -0700

    gameroom

--- spotifight-ui/src/components/GameRoom/GameRoom.jsx
+++ spotifight-ui/src/components/GameRoom/GameRoom.jsx
@@ -21,7 +21,7 @@ const mapDispatchToProps = function(dispatch) {
 };
 
 const games = {
-  'Masher': Masher
+  Masher: Masher
 };
 class GameRoom extends Component {
   constructor(props) {
@@ -31,19 +31,16 @@ class GameRoom extends Component {
       test: "",
       currRoom: Lobby,
       players: players,
-      socketID: '',
-      localUser: 'MikeUser',
-      userImg: 'https://lh3.googleusercontent.com/-tcP7CBn3lpg/Tg15KKkK6pI/AAAAAAAAABQ/Hph0kqR-hKU/w530-h530-n-rw/photo.jpg',
-      winner: '',
+      socketID: "",
+      localUser: "Nelson",
+      userImg:
+        "https://lh3.googleusercontent.com/-tcP7CBn3lpg/Tg15KKkK6pI/AAAAAAAAABQ/Hph0kqR-hKU/w530-h530-n-rw/photo.jpg",
+      winner: "",
 
       selectedGame: this.props.game
     };
 
     this.getWinner = this.getWinner.bind(this);
-
-
-
-
   }
   async componentWillMount() {
     this.socket = await io.connect("http://localhost:8000", {
@@ -54,13 +51,12 @@ class GameRoom extends Component {
       this.setState({ currRoom: games[data] });
     });
 
-    this.state.socket.on('finalScoreObject', (finalScore)=> {
-      console.log(finalScore, 'HERE IS THE FINAL SCORE');
-    var winner =  this.getWinner(finalScore);
-    this.setState({winner: winner})
-    this.state.socket.emit('broadcastWinner', winner);
-  });
-
+    this.state.socket.on("finalScoreObject", finalScore => {
+      console.log(finalScore, "HERE IS THE FINAL SCORE");
+      var winner = this.getWinner(finalScore);
+      this.setState({ winner: winner });
+      this.state.socket.emit("broadcastWinner", winner);
+    });
   }
 
   startGame() {
@@ -69,22 +65,25 @@ class GameRoom extends Component {
   }
 
   getWinner(final) {
-    console.log(final, 'in final score')
-      let values = Object.entries(final);
-    values = values.sort((a, b)=> {
-        return b[1] - a[1];
-      })
-      console.log(values[0], '<------HERE IS YOUR WINNER')
-      return values[0];
+    console.log(final, "in final score");
+    let values = Object.entries(final);
+    values = values.sort((a, b) => {
+      return b[1] - a[1];
+    });
+    console.log(values[0], "<------HERE IS YOUR WINNER");
+    return values[0];
+
+
   }
 
   render() {
     return (
       <div>
-        <this.state.currRoom socket={this.state.socket}
-        userImg={this.state.userImg}
-        localUser={this.state.localUser}
-        winner = {this.state.winner}
+        <this.state.currRoom
+          socket={this.state.socket}
+          userImg={this.state.userImg}
+          localUser={this.state.localUser}
+          winner={this.state.winner}
         />
         <Grid container>
           <Grid item md={5} />
@@ -99,9 +98,9 @@ class GameRoom extends Component {
                 START GAME
               </Button>
             )}
-
           </Grid>
         </Grid>
+        
       </div>
     );
   }
