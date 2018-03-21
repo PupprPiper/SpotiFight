94eb32639ef6c14b7b8c4ea55c5e5556db9cc717
Ryan Santos <ryansantos86@gmail.com>
2018-03-20 17:11:20 -0700

    (feat) auth data in response

--- server/environment/keys.js
+++ server/environment/keys.js
@@ -3,5 +3,8 @@ module.exports = {
     clientID:
       '668624890134-6k5g07fkdmfa3djtvpgf6sda4hivm4tm.apps.googleusercontent.com',
     clientSecret: '8tpG-M5RkYDOmQ262IOh7rbR'
+  },
+  session: {
+    cookieKey: 'spotifightIsAwesome'
   }
 };
