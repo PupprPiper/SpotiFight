34e08acf88b4a349539a5d5846ed5eb3c5a26300
Michael Wachell <wachell@gmail.com>
2018-03-24 12:45:37 -0700

    refactored sockets to clean them up

--- server/socket-server/src/index.js
+++ server/socket-server/src/index.js
@@ -1,71 +1,68 @@
-require("babel-register");
-require("babel-polyfill");
-const express = require("express");
+require('babel-register');
+require('babel-polyfill');
+const express = require('express');
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const Masher = require("./masher");
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Masher = require('./masher');
 
-let userObject = {};
+
+
 
 let users = [];
 let connections = [];
 let masherGame = {};
 
-io.on("connection", client => {
-  let roomId = client.handshake.query.roomId;
+io.on('connection', client => {
+
   if (client.handshake.query.roomId) {
-    client.join(client.handshake.query.roomId);
-    if (!userObject[roomId]) {
-      userObject[roomId] = [];
-    }
-    console.log("new user has joined room: ", client.handshake.query.roomId);
-    
+    client.join(client.handshake.query.roomId)
+    console.log('new user has joined room: ', client.handshake.query.roomId);
   }
   connections.push(client);
   console.log(`Connected %s clients connected ${connections.length}`);
 
   // disconnect
-  client.on("disconnect", data => {
+  client.on('disconnect', data => {
     connections.splice(connections.indexOf(client), 1);
 
     console.log(`Disconnected: %s clients connected ${connections.length}`);
   });
 
   // send message
-  client.on("send message", data => {
+  client.on('send message', data => {
     console.log(data);
-    io.in(client.handshake.query.roomId).emit("newMessage", {
+    io.in(client.handshake.query.roomId).emit('newMessage', {
       msg: data
     });
   });
 
-  client.on("startGameHost", data => {
+  client.on('startGameHost', data => {
     console.log(data);
-    io.in(client.handshake.query.roomId).emit("startGameAll", data);
+    io.in(client.handshake.query.roomId).emit('startGameAll', data)
   });
 
-  client.on("broadcastWinner", data => {
+  client.on('broadcastWinner', data => {
     console.log(data);
-    io.in(client.handshake.query.roomId).emit("receiveWinner", data);
+    io.in(client.handshake.query.roomId).emit('receiveWinner', data)
   });
 
-  client.on("SEND_WINNER_SONG", data => {
-    io.in(client.handshake.query.roomId).emit("GLOBAL_SONG", data);
+  client.on('SEND_WINNER_SONG', data => {
+    io.in(client.handshake.query.roomId).emit('GLOBAL_SONG', data)
   });
 
-  client.on("USER_ENTER_LOBBY", user => {
-    userObject[roomId].push(user);
-    console.log("userObject", userObject);
-    io.in(client.handshake.query.roomId).emit("newMessage", {
+  client.on('USER_ENTER_LOBBY',  user => {
+    users.push(user)
+    io.in(client.handshake.query.roomId).emit('newMessage', {
       msg: `${user.username} has entered lobby`
     });
-    console.log(`${user.username} has entered lobby`);
-    client.on("disconnect", data => {
-      userObject[roomId].splice(userObject[roomId].indexOf(user.username), 1);
-      io.in(client.handshake.query.roomId).emit("newMessage", {
+    console.log(`${user.username} has entered lobby`)
+    client.on('disconnect', data =>{
+      users.splice(users.indexOf(user.username), 1)
+      io.in(client.handshake.query.roomId).emit('newMessage', {
         msg: `${user.username} has disconnected`
       });
+<<<<<<< HEAD
 <<<<<<< HEAD
       io
         .in(client.handshake.query.roomId)
@@ -79,22 +76,16 @@ io.on("connection", client => {
       .in(client.handshake.query.roomId)
       .emit("ACTIVE_USERS", userObject[roomId]);
   });
+=======
+      io.in(client.handshake.query.roomId).emit('ACTIVE_USERS', users)
+    })
+    io.in(client.handshake.query.roomId).emit('ACTIVE_USERS', users)
+  })
+>>>>>>> refactored sockets to clean them up
 
-  // MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER
+// MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER
 
-  client.on("updateScore", data => {
-    if (!masherGame.hasOwnProperty(data.localUser)) {
-      masherGame[data.localUser] = 1;
-    } else {
-      masherGame[data.localUser] += 1;
-    }
-    console.log(masherGame);
-    io.in(client.handshake.query.roomId).emit("displayUpdate", {
-      player: data.localUser,
-      score: masherGame
-    });
-  });
-
+<<<<<<< HEAD
   client.on("clearBoard", data => {
     masherGame = {};
   });
@@ -116,10 +107,13 @@ io.on("connection", client => {
 
 // MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER  MASHER
 
+=======
+>>>>>>> refactored sockets to clean them up
  Masher.updateScore(client, users, connections, masherGame, io);
  Masher.clearBoard(client, users, connections, masherGame, io);
  Masher.buildBoard(client, users, connections, masherGame, io);
  Masher.finalScore(client, users, connections, masherGame, io)
+<<<<<<< HEAD
 >>>>>>> refactored sockets to clean them up
 
   client.on("finalScore", data => {
@@ -127,9 +121,10 @@ io.on("connection", client => {
     io.in(client.handshake.query.roomId).emit("finalScoreObject", finalScore);
     console.log("finalScore firing on server", finalScore);
   });
+=======
+>>>>>>> refactored sockets to clean them up
 
-  // END MASHER END MASHER END MASHER END MASHER END MASHER END MASHER END MASHER
-});
+})
 
 const PORT = 8000;
 http.listen(PORT, () => console.log(`socket server listening on port ${PORT}`));
