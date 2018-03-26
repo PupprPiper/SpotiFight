1bb1a46ecc6b5d1479a27f5eecc40ae537c924de
Johnny Siu <siujohnny@gmail.com>
2018-03-26 11:44:37 -0700

    working on sockets

--- server/socket-server/src/rpsls.js
+++ server/socket-server/src/rpsls.js
@@ -7,8 +7,7 @@ const http = require('http').Server(app);
 module.exports = {
   makeChoice : (client, users, connections, masherGame, io) => {
     client.on('makeChoice', data => {
-      console.log('MAKE CHOICE DATA ', data)
-      io.in(client.handshake.query.roomId).emit('something else', 'stuff you want to emit')
+      io.in(client.handshake.query.roomId).emit('oppChoice', data)
     })
   }
 }
--- spotifight-ui/src/components/Games/RPSLS/rpsls.jsx
+++ spotifight-ui/src/components/Games/RPSLS/rpsls.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './rpsls.scss';
+import "./rpsls.scss";
 export default class rpsls extends Component {
   constructor(props) {
     super(props);
@@ -28,43 +28,54 @@ export default class rpsls extends Component {
         scissorsrock: "LOSE!"
       },
       images: {
-        rock: 'https://i.imgur.com/z8yK0Q4.jpg',
-        paper: 'https://i.imgur.com/wXq7nj8.jpg',
-        scissors: 'https://i.imgur.com/jljkva5.jpg',
-        lizard: 'https://i.imgur.com/6ix7L4A.jpg',
-        spock: 'https://i.imgur.com/sRHZvfq.jpg'
-
+        rock: "https://i.imgur.com/z8yK0Q4.jpg",
+        paper: "https://i.imgur.com/wXq7nj8.jpg",
+        scissors: "https://i.imgur.com/jljkva5.jpg",
+        lizard: "https://i.imgur.com/6ix7L4A.jpg",
+        spock: "https://i.imgur.com/sRHZvfq.jpg"
       },
       localUser: this.props.localUser,
       players: this.props.players,
-      socket: this.props.socket
+      socket: this.props.socket,
+      userChoice: null,
+      oppChoice: null
     };
     this.makeChoice = this.makeChoice.bind(this);
   }
   componentDidMount() {
-    console.log('players list here ', this.state.players)
+    this.state.socket.on("oppChoice", data => {
+      if (data.user !== this.state.localUser) {
+        this.setState({ oppChoice: data.choice });
+
+      }
+    });
   }
 
   async makeChoice(i) {
-    console.log(this.state.localUser, this.state.choices[i])
-   this.state.socket.emit('makeChoice', {
-     user: this.state.localUser,
-     choice: this.state.choices[i]
-    })
+    this.setState({userChoice: this.state.choices[i]})
+    this.state.socket.emit("makeChoice", {
+      user: this.state.localUser,
+      choice: this.state.choices[i]
+    });
   }
 
   render(props) {
     return (
       <div>
+        <div />
         <div>
-
-        </div>
-        <div>
-        {this.state.choices.map((choice, i) => {
-          return (
-            <img className="choices" key={i} index={i} onClick={() => this.makeChoice(i)} src={this.state.images[choice]} alt={choice}/>
-          );
-        })}
+          {this.state.choices.map((choice, i) => {
+            return (
+              <img
+                className="choices"
+                key={i}
+                index={i}
+                onClick={() => this.makeChoice(i)}
+                src={this.state.images[choice]}
+                alt={choice}
+              />
+            );
+          })}
         </div>
       </div>
     );
