19b32a628a404ee72f0c7398afd8622c24f53d43
Michael Wachell <michaelwachell@Michaels-Mac-Pro.local>
2018-03-29 18:43:29 -0700

    branch culling

--- spotifight-ui/src/routes.js
+++ spotifight-ui/src/routes.js
@@ -57,60 +57,22 @@ const appRoutes = [
     sidebarName: 'Login',
     icon: AccountBox,
     component: Login
-  }, {
-    path: '/chat',
-    sidebarName: 'Chat',
-    icon: Person,
-    component: Chat,
-    protected: true
-  }, {
-    path: '/game-room',
-    sidebarName: 'Game Room',
-    icon: Gamepad,
-    component: GameRoom,
-    protected: true
-  }, {
-    path: '/games',
-    sidebarName: 'Games',
-    icon: Gamepad,
-    component: Games,
-    protected: true
-  }, {
-    path: '/landing-page',
-    sidebarName: 'Landing Page',
-    icon: Dashboard,
-    component: LandingPage
-  }, {
+  },    
+    {
     path: '/leader-board',
     sidebarName: 'Leader Board',
     icon: Accessibility,
     component: LeaderBoard,
     protected: true
-  }, {
-    path: '/lobby',
-    sidebarName: 'Lobby',
-    icon: Room,
-    component: Lobby,
-    protected: true
-  }, {
-    path: '/music-player',
-    sidebarName: 'Music Player',
-    icon: MusicNote,
-    component: MusicPlayer,
-    protected: true
-  }, {
+  },
+   {
     path: `/user-profile/`,
     sidebarName: 'User Profile',
     icon: AccountBox,
     component: UserProfile,
     protected: true
-  }, {
-    path: '/flappy',
-    sidebarName: 'Flappy',
-    icon: AccountBox,
-    component: Flappy,
-    store: store
-  }, {
+  },
+     {
     redirect: true,
     path: '/',
     to: '/home'
