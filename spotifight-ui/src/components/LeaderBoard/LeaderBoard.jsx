b151c4710cc256f3ade944eb004347a0cbdf73f7
Johnny Siu <siujohnny@gmail.com>
2018-04-03 11:22:08 -0700

    leaderboard css

--- spotifight-ui/src/components/LeaderBoard/LeaderBoard.jsx
+++ spotifight-ui/src/components/LeaderBoard/LeaderBoard.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Verify from '../Auth/Verify.jsx'
 import axios from "axios";
-
+import Table from './Table.jsx';
+import './LeaderBoard.scss';
 
 export default class LeaderBoard extends Component {
   constructor() {
@@ -13,45 +14,18 @@ export default class LeaderBoard extends Component {
 
   async componentDidMount() {
     var data = await axios.get("http://localhost:3000/users/fetchAllUsers");
-    console.log("DATA HERE ", data);
     data.data.sort((a, b) => {
       return b.wins - a.wins;
     });
 
-    console.log("DATA HERE ", data.data);
     await this.setState({ users: data.data });
-    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        <h3>LEADERBOARD</h3>
-        {this.state.users ?
-        (<table align="center">
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Username</th>
-              <th>Wins</th>
-              <th>Losses</th>
-            </tr>
-          </thead>
-          <tbody>
-            {console.log("INSIDE RENDER ", this.state.users)}
-            {this.state.users.map((user, i) => {
-              return (
-                <tr key={i}>
-                  <th>{i + 1}</th>
-                  <th>{user.username}</th>
-                  <th>{user.wins}</th>
-                  <th>{user.losses}</th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>):null
-        }
+        <Table/>
+
               <Verify history={this.props.history}  />
       </div>
     );
