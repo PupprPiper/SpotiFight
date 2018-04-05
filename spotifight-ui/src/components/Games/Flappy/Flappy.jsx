c837b7fb70b95d0fe15b434b849e7185eeb75302
Johnny Siu <siujohnny@gmail.com>
2018-04-05 10:31:06 -0700

    saving

--- spotifight-ui/src/components/Games/Flappy/Flappy.jsx
+++ spotifight-ui/src/components/Games/Flappy/Flappy.jsx
@@ -21,6 +21,7 @@ import {
   winnerStyle
 } from './gameHelpers';
 import PlayerStatus from './playerStatus';
+import axios from 'axios';
 
 class Flappy extends Component {
   constructor(props) {
@@ -124,11 +125,15 @@ class Flappy extends Component {
       this.setState({ winner: data.winner });
       if (this.props.localUser === data.winner) {
         this.props.socket.emit('SEND_WINNER_SONG', this.props.mySong);
-        
+
         this.props.players.forEach((player => {
+          console.log(player.username, data.winner)
           if(player.username !== data.winner){
+            console.log('GETS HERE')
+            
             axios.put('http://localhost:3000/users/addWinLoss', {field: 'losses', user_id: player.id })
           }else{
+            console.log('ELSE HERE')
             axios.put('http://localhost:3000/users/addWinLoss', {field: 'wins', user_id: player.id })
           }
         }))
