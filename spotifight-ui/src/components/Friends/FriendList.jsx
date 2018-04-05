583b56244ad38d3f93679b92086c53be8238f4ef
Johnny Siu <siujohnny@gmail.com>
2018-04-04 17:41:18 -0700

    saving progress

--- spotifight-ui/src/components/Friends/FriendList.jsx
+++ spotifight-ui/src/components/Friends/FriendList.jsx
@@ -12,7 +12,12 @@ import {
   Checkbox,
   Paper
 } from './../Global/Material-Globals';
-import { requestFriend, acceptFriend, removeFriend, rejectFriend } from './FriendListHelpers';
+import {
+  requestFriend,
+  acceptFriend,
+  removeFriend,
+  rejectFriend
+} from './FriendListHelpers';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
@@ -35,7 +40,7 @@ class FriendList extends Component {
   }
 
   async fetchAllFriends() {
-    console.log('PROPS FROM FRIEND LIST ', this.props)
+    console.log('PROPS FROM FRIEND LIST ', this.props);
     var allFriends = await axios.get(
       `http://localhost:3000/friends/fetchAllFriends/${
         this.props.userProfile.id
@@ -70,20 +75,33 @@ class FriendList extends Component {
   render() {
     return (
       <div>
-        <Grid style={{marginTop: '5%'}} container spacing={24}>
+        <Grid style={{ marginTop: '5%' }} container spacing={24}>
           <Grid align="center" item xs={12} md={4}>
-            <h3>Your Friends</h3>
-             <List>
-              {this.props.friends
-                ? this.props.friends.map((friend, i) => {
-                    return (
-                      <Paper key={i} >
-                      <FriendListItem fetchAllFriends={this.fetchAllFriends} friend={friend}/>
-                      </Paper>
-                    );
-                  })
-                : null}
-            </List>
+            <div className="friend-table top-margin">
+              <h3 className="friend-head"> Friends </h3>
+              <table align="left" style={{ width: '100%' }}>
+                <thead>
+                  <tr>
+                    <th>Avatar</th>
+                    <th>Username</th>
+                    <th>Wins</th>
+                    <th>Losses</th>
+                    <th>Delete?</th>
+                  </tr>
+                </thead>
+                {this.props.friends
+                  ? this.props.friends.map((friend, i) => {
+                      return (
+                        <FriendListItem
+                          key={i}
+                          fetchAllFriends={this.fetchAllFriends}
+                          friend={friend}
+                        />
+                      );
+                    })
+                  : null}
+              </table>
+            </div>
           </Grid>
           <Grid align="center" item xs={12} md={4}>
             <h3>Pending Friend Requests</h3>
@@ -92,7 +110,10 @@ class FriendList extends Component {
                 ? this.props.pendingFriends.map((friend, i) => {
                     return (
                       <Paper key={i}>
-                      <PendingFriendListItem fetchAllFriends={this.fetchAllFriends} friend={friend}/>
+                        <PendingFriendListItem
+                          fetchAllFriends={this.fetchAllFriends}
+                          friend={friend}
+                        />
                       </Paper>
                     );
                   })
@@ -116,7 +137,10 @@ class FriendList extends Component {
             {this.props.filteredUsers
               ? this.props.filteredUsers.map((user, i) => {
                   return (
-                    <SearchListItem fetchAllFriends={this.fetchAllFriends} user={user}/>
+                    <SearchListItem
+                      fetchAllFriends={this.fetchAllFriends}
+                      user={user}
+                    />
                   );
                 })
               : null}
@@ -152,3 +176,61 @@ const mapDispatchToProps = function(dispatch) {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FriendList);
+
+{
+  /* 
+<div>
+<Grid style={{marginTop: '5%'}} container spacing={24}>
+  <Grid align="center" item xs={12} md={4}>
+    <h3>Your Friends</h3>
+     <List>
+      {this.props.friends
+        ? this.props.friends.map((friend, i) => {
+            return (
+              <Paper key={i} >
+              <FriendListItem fetchAllFriends={this.fetchAllFriends} friend={friend}/>
+              </Paper>
+            );
+          })
+        : null}
+    </List>
+  </Grid>
+  <Grid align="center" item xs={12} md={4}>
+    <h3>Pending Friend Requests</h3>
+    <List>
+      {this.props.pendingFriends
+        ? this.props.pendingFriends.map((friend, i) => {
+            return (
+              <Paper key={i}>
+              <PendingFriendListItem fetchAllFriends={this.fetchAllFriends} friend={friend}/>
+              </Paper>
+            );
+          })
+        : null}
+    </List>
+  </Grid>
+  <Grid align="center" item xs={12} md={4}>
+    <input
+      type="text"
+      value={this.props.searchInput}
+      onChange={async e => {
+        await this.props.updateSearchInput(e.target.value);
+        if (this.props.searchInput.length > 2) {
+          this.filterUsers();
+        } else {
+          this.props.updateFilteredUsers(null);
+        }
+      }}
+    />
+    <h5>Search Results</h5>
+    {this.props.filteredUsers
+      ? this.props.filteredUsers.map((user, i) => {
+          return (
+            <SearchListItem fetchAllFriends={this.fetchAllFriends} user={user}/>
+          );
+        })
+      : null}
+  </Grid>
+</Grid>
+</div> */
+}
--- spotifight-ui/src/components/Friends/FriendListItem.jsx
+++ spotifight-ui/src/components/Friends/FriendListItem.jsx
@@ -15,6 +15,7 @@ import {
   updateAllUsers,
   updateSearchInput
 } from '../../actions/index';
+import './FriendList.scss';
 
 class FriendListItem extends Component {
     constructor(props){
@@ -23,52 +24,78 @@ class FriendListItem extends Component {
     render(){
 
         return (
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src={this.props.friend.avatar_url} alt="Image" />
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong style={{ fontSize: '20px' }}>{this.props.friend.username}</strong>
-                  <small style={{ float: 'right' }}>{this.props.friend.status}</small>
-                  <br />
-                  <small>
-                    wins: {this.props.friend.wins} losses: {this.props.friend.losses}
-                  </small>
-                </p>
-                  <Button variant="raised" color="primary"
-                    onClick={async () => {
+      //     <div className="friend-table top-margin">
+      // <h3 className="friend-head"> Friends </h3>
+      //   <table align="center" style={{ width: "40%" }}>
+      //   <thead>
+      //   <tr>
+      //     <th>Avatar</th>
+      //     <th>Username</th>
+      //     <th>Wins</th>
+      //     <th>Losses</th>
+      //   </tr>
+      // </thead>
+      
+          <tbody>
+              <tr>
+                <td><img src={this.props.friend.avatar_url} alt="avatar" width="50" height="50" className="table-img"/></td>
+                <td>{this.props.friend.username}</td>
+                <td>{this.props.friend.wins}</td>
+                <td>{this.props.friend.losses}</td>
+                <td><button onClick={async () => {
                       await removeFriend(this.props.userProfile.id, this.props.friend.id);
                       this.props.fetchAllFriends();
-                    }}
-                  >
-                    Remove Friend
-                  </Button>
-              </div>
-              <nav className="level">
-                <div className="level-left">
-                  <a className="level-item">
-                    <span className="icon is-small">
-                      <i className="fa fa-reply" />
-                    </span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-small">
-                      <i className="fa fa-retweet" />
-                    </span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-small">
-                      <i className="fa fa-heart" />
-                    </span>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </article>
+                    }} className="delete" type="button">Delete</button></td>
+              </tr>
+          </tbody>
+    
+
+        //   <article className="media">
+        //     <div className="media-left">
+        //       <figure className="image is-64x64">
+        //         <img src={this.props.friend.avatar_url} alt="Image" />
+        //       </figure>
+        //     </div>
+        //     <div className="media-content">
+        //       <div className="content">
+        //         <p>
+        //           <strong style={{ fontSize: '20px' }}>{this.props.friend.username}</strong>
+        //           <small style={{ float: 'right' }}>{this.props.friend.status}</small>
+        //           <br />
+        //           <small>
+        //             wins: {this.props.friend.wins} losses: {this.props.friend.losses}
+        //           </small>
+        //         </p>
+        //           <Button variant="raised" color="primary"
+        //             onClick={async () => {
+        //               await removeFriend(this.props.userProfile.id, this.props.friend.id);
+        //               this.props.fetchAllFriends();
+        //             }}
+        //           >
+        //             Remove Friend
+        //           </Button>
+        //       </div>
+        //       <nav className="level">
+        //         <div className="level-left">
+        //           <a className="level-item">
+        //             <span className="icon is-small">
+        //               <i className="fa fa-reply" />
+        //             </span>
+        //           </a>
+        //           <a className="level-item">
+        //             <span className="icon is-small">
+        //               <i className="fa fa-retweet" />
+        //             </span>
+        //           </a>
+        //           <a className="level-item">
+        //             <span className="icon is-small">
+        //               <i className="fa fa-heart" />
+        //             </span>
+        //           </a>
+        //         </div>
+        //       </nav>
+        //     </div>
+        //   </article>
         );
       };
     }
